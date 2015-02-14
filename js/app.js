660de33cff8a7632662afa9712b195beeea83e2f
vamdt <liang6688267@gmail.com>
2015-02-14 15:56:00 +0000

    Add lose logic

--- js/app.js
+++ js/app.js
@@ -5,10 +5,12 @@ define(['zepto'], function($) {
   }
   $(function() {
     var body_height = $(document).height(),
-        cell_height = body_height / 4;
+        cell_height = body_height / 4,
+        quarter_cell_height = cell_height / 4;
     $('table').height(body_height);
     $('table tr').each(function(index) {
         $(this).height(cell_height);
+        if(index === 3) return;
         $(this).children().eq(rand4()).addClass('target');
     });
     for (var i = 100; i > 0; i--) {
@@ -16,9 +18,20 @@ define(['zepto'], function($) {
         el.height(cell_height).children().eq(rand4()).addClass('target');
         $('table tbody').prepend(el);
     };
-    $('table').css('top', -100 * cell_height);
+    var initialTop  = (-$('table').height())+(4 * cell_height);
+    $('table').css('top', initialTop);
+    var counter = 0, cell_counter=0, temp_height, bit, step = 23;
     var cancelInterval = setInterval(function(){
-        $('table').css('top', ($('table').offset().top + 5) + 'px');
+        $('table').css('top', ($('table').offset().top + step) + 'px');
+        counter += 1;
+        bit = (counter * step) / cell_height;
+        cell_counter = Math.ceil(bit);
+        temp_height = parseFloat( bit.toString().replace(/^\d+\./, '0.') );
+        if (temp_height > 0.25 &&
+            $('tr').eq(-cell_counter).has('td.target').length) {
+            clearInterval(cancelInterval);
+            alert('You Loose!')
+        };
     }, 80);
 
 
