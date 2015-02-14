62d627701bc47e7dc270826a6988ba54f6cc8570
vamdt <liang6688267@gmail.com>
2015-02-14 14:24:44 +0800

    scroll

--- js/app.js
+++ js/app.js
@@ -4,8 +4,19 @@ define(['zepto'], function($) {
     return Math.floor(Math.random() * 4 );
   }
   $(function() {
+    var body_height = $(document).height(),
+        cell_height = body_height / 4;
+    $('table').height(body_height);
     $('table tr').each(function(index) {
-      $(this).children().eq(rand4()).addClass('target');
+        $(this).height(cell_height);
+        $(this).children().eq(rand4()).addClass('target');
     });
+    var el;
+    setInterval(function() {
+        el = $('<tr><td></td><td></td><td></td><td></td></tr>');
+        el.height(cell_height).children().eq(rand4()).addClass('target');
+        $('table tbody').prepend(el);
+    }, 300);
+
   });
 });
